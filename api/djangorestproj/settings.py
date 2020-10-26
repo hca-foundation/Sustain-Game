efb58ab00fc54e1d03ca84a365e0eea19c1c9c66
Trinity Terry <trinitycterry@gmail.com>
2020-10-25 21:49:21 -0500

    added postgres settings to api

--- api/djangorestproj/settings.py
+++ api/djangorestproj/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'djangorestproj.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
