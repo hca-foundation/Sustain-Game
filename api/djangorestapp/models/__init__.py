041312c5116103e0767cf6f74e06d17b3d507708
Trinity Terry <trinitycterry@gmail.com>
2020-10-27 10:57:20 -0500

    Added Quiz Bank Model

--- api/djangorestapp/models/__init__.py
+++ api/djangorestapp/models/__init__.py
@@ -1,1 +1,2 @@
 from .event import Event
+from .quiz_bank import QuizBank
---
+++ api/djangorestapp/models/quiz_bank.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+class QuizBank(models.Model):
+    """Quiz Bank Model
+    
+    Description: Stores a single quiz for the quiz bank
+
+    Fields
+    - name = `models.CharField(max_length=255)`
+    - timer = `models.IntegerField`
+    """
+    name = models.CharField(max_length=255)
+    timer = models.IntegerField()
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name_plural = "quiz banks"
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
