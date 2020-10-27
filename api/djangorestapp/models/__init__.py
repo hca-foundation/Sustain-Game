7fd487216810d75322aa68e2af701a791de76620
Trinity Terry <trinitycterry@gmail.com>
2020-10-27 16:38:43 -0500

    Created QuestionBank Model

--- api/djangorestapp/models/__init__.py
+++ api/djangorestapp/models/__init__.py
@@ -1,2 +1,3 @@
 from .event import Event
 from .quiz_bank import QuizBank
+from .question_bank import QuestionBank
---
+++ api/djangorestapp/models/question_bank.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+class QuestionBank(models.Model):
+    """Question Bank Model
+
+    Fields
+    - question = `models.CharField(max_length=255)`
+    - image = `models.URLField(max_length=200, blank=True, null=True)`
+    - value = `models.IntegerField`
+    - info_link = `models.URLField(max_length=200, blank=True, null=True)`
+    """
+    question = models.CharField(max_length=255)
+    image = models.URLField(max_length=200, blank=True, null=True)
+    value = models.IntegerField(default=1)
+    info_link = models.URLField(max_length=200, blank=True, null=True)
+
+
+    class Meta:
+        ordering = ["question"]
+        verbose_name_plural = "Question Banks"
+
+    def __str__(self):
+        return self.question
+
+    def __unicode__(self):
+        return self.question
+
+    
