bd63cd80e235e22182f4f70912b9066583e85343
Trinity Terry <trinitycterry@gmail.com>
2020-10-26 21:50:50 -0500

    Added Event Model

---
+++ api/djangorestapp/models/event.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Event(models.Model):
+    """Event Model
+
+    Fields
+    - name = `models.CharField`
+    - active =  `models.BooleanField`
+    - date = `models.DateField(auto_now=False, auto_now_add=False)`
+    """
+    name = models.CharField(max_length=255)
+    active = models.BooleanField(default=False)
+    child_mode = models.BooleanField(default=False)
+    date = models.DateField(auto_now=False, auto_now_add=False)
+
+    class Meta:
+        ordering = ["active"]
+        verbose_name_plural = "events"
+
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return self.name
+
+    
