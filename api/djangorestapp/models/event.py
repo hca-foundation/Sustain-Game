07180339d6193e588c3a7dbb6d9edcc1dc4542fb
Trinity Terry <trinitycterry@gmail.com>
2020-10-27 19:58:27 -0500

    Added safedelete to quiz bank and event models

--- api/djangorestapp/models/event.py
+++ api/djangorestapp/models/event.py
@@ -1,16 +1,22 @@
 from django.db import models
+from safedelete.models import SafeDeleteModel
+from safedelete.models import HARD_DELETE_NOCASCADE
 
-
-class Event(models.Model):
+class Event(SafeDeleteModel):
     """Event Model
 
     Description: Stores a single event
+
+    If an Event is deleted it is just masked so that it can still be accessed from the QuizTaker Model.
 
     Fields
     - name = `CharField`
     - active =  `BooleanField`
     - date = `DateField(auto_now=False, auto_now_add=False)`
     """
+    # Added this because the important quiz_taker model uses
+    _safedelete_policy = HARD_DELETE_NOCASCADE
+
     name = models.CharField(max_length=255)
     active = models.BooleanField(default=False)
     child_mode = models.BooleanField(default=False)
--- api/djangorestapp/models/quiz_bank.py
+++ api/djangorestapp/models/quiz_bank.py
@@ -1,4 +1,6 @@
 from django.db import models
+from safedelete.models import SafeDeleteModel
+from safedelete.models import HARD_DELETE_NOCASCADE
 
 
 class QuizBank(models.Model):
@@ -6,10 +8,15 @@ class QuizBank(models.Model):
 
     Description: Stores a single quiz for the quiz bank
 
+    If a Quiz is deleted it is just masked so that it can still be accessed from the QuizTaker Model.
+
     Fields
     - name = `CharField(max_length=255)`
     - timer = `IntegerField` in minutes
     """
+    # Added this because the important quiz_taker model uses
+    _safedelete_policy = HARD_DELETE_NOCASCADE
+
     name = models.CharField(max_length=255)
     timer = models.IntegerField()
 
