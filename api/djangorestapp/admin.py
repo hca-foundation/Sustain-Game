97c873f23e9250ab40f98fae46cae8f6e4aba69c
Trinity Terry <trinitycterry@gmail.com>
2020-10-28 12:55:56 -0500

    Registered Admin Models

--- api/djangorestapp/admin.py
+++ api/djangorestapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+
+admin.site.register(models.EventQuiz)
+admin.site.register(models.Event)
+admin.site.register(models.QuestionBankAnswer)
+admin.site.register(models.QuestionBank)
+admin.site.register(models.QuizBank)
+admin.site.register(models.QuizQuestion)
+admin.site.register(models.QuizTaker)
