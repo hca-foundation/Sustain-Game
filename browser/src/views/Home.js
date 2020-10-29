4f05d1994ed37683f36f1b9b2f967278d55d3612
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-10-29 11:43:53 -0500

    added required fields and validation on zip

--- browser/src/views/Home.js
+++ browser/src/views/Home.js
@@ -25,20 +25,20 @@ export default class Home extends Component {
     return (
       <Form onSubmit={this.handleSubmit}>
         <FormGroup>
-          <Label for="fname">First Name</Label>
-          <Input onChange={this.handleChange} type="text" name="fname" id="fname" placeholder="First Name" />
+          <Label for="fname">First Name <span className="form-required">*</span></Label>
+          <Input onChange={this.handleChange} type="text" name="fname" id="fname" placeholder="First Name" required/>
         </FormGroup>
         <FormGroup>
-          <Label for="lname">Last Name</Label>
-          <Input onChange={this.handleChange} type="text" name="lname" id="lname" placeholder="Last Name" />
+          <Label for="lname">Last Name <span className="form-required">*</span></Label>
+          <Input onChange={this.handleChange} type="text" name="lname" id="lname" placeholder="Last Name" required/>
         </FormGroup>
         <FormGroup>
-          <Label for="email">Email</Label>
-          <Input onChange={this.handleChange} type="email" name="email" id="email" placeholder="youremail@email.com" />
+          <Label for="email">Email <span className="form-required">*</span></Label>
+          <Input onChange={this.handleChange} type="email" name="email" id="email" placeholder="youremail@email.com" required/>
         </FormGroup>
         <FormGroup>
-          <Label for="lname">Zip Code</Label>
-          <Input onChange={this.handleChange} type="text" name="zip" id="zip" placeholder="Zip Code" />
+          <Label for="lname">Zip Code (Optional)</Label>
+          <Input onChange={this.handleChange} type="number" pattern="[0-9]*" name="zip" id="zip" placeholder="Zip Code" />
         </FormGroup>
         <Button>Submit</Button>
       </Form>
