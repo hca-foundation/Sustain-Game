166aabb595fa3e14aa6d1ee36f08c78b395a697e
CGI7188 <CGI7188@CORPCGI7188MP.hsd1.tn.comcast.net>
2020-10-29 17:19:40 -0500

    modal styling

--- browser/src/components/Modal/index.js
+++ browser/src/components/Modal/index.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import {
   Button, Modal, ModalHeader, ModalBody,
 } from 'reactstrap';
+import sustaingame from '../../images/sustaingame.png';
 
 const GameModal = (props) => {
   const {
@@ -20,9 +21,12 @@ const GameModal = (props) => {
     <div>
       <Button className="btn-transparent" color="info" onClick={toggle}>{buttonLabel}</Button>
       <Modal isOpen={modal} toggle={toggle} className={className}>
-        <ModalHeader toggle={toggle} close={closeBtn}>How to play</ModalHeader>
+        <ModalHeader toggle={toggle} close={closeBtn}></ModalHeader>
         <ModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <img src={sustaingame} alt="sustain game" class="sustaingame-img"/>
+          <h1>How to Play</h1>
+          <p>Aenean lacinia bibendum nulla sed consectetur. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis</p>
+          <p>Aenean lacinia bibendum nulla sed consectetur. Nullam id dolor id nibh ultricies vehicula ut id elit. Cum sociis natoque penatibus et magnis dis</p>
         </ModalBody>
       </Modal>
     </div>
