7dcf77ce329d011fe5f5cb3fce44ed0e34bebbbd
Trinity Terry <trinitycterry@gmail.com>
2020-10-28 20:56:15 -0500

    Added quiz taker route

---
+++ api/djangorestapp/views/quiz_taker.py
@@ -0,0 +1,26 @@
+from django.http import HttpResponse
+from rest_framework import serializers, status
+from djangorestapp.models import QuizTaker
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+
+@api_view(('POST',))
+def post_quiz_taker(request):
+    try:
+        data = request.data
+        serialized_data = QuizTakerSerializer(data=data)
+        is_valid = serialized_data.is_valid()
+        if not is_valid:
+            return Response({"detail": serialized_data.errors}, status=status.HTTP_400_BAD_REQUEST)
+        serialized_data.save()
+        return Response(serialized_data.data)
+    except Exception as error:
+        return Response({"detail": f"{error}"}, status=status.HTTP_404_NOT_FOUND)
+
+
+class QuizTakerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuizTaker
+        fields = ["email", "fname", "lname", "event",
+                  "quiz_bank", "score", "initials"]
