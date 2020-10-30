af8bfd9d093db6af5c939ef641e29f9ae9b9d603
Prafullata Sonawane <prafullatass@gmail.com>
2020-10-29 19:55:00 -0500

    added score, initials and thanks components

--- browser/src/components/App/index.js
+++ browser/src/components/App/index.js
@@ -4,6 +4,9 @@ import getActiveEventQuestions from '../../data/getActiveEvent';
 import Home from '../../views/Home';
 import Splash from '../../views/Splash';
 import Countdown from '../../views/Countdown';
+import Score from '../views/Score';
+import Thanks from '../views/Thanks';
+import Initials from '../views/Initials';
 
 export default class App extends Component {
   state = {
@@ -32,10 +35,25 @@ export default class App extends Component {
             exact
             component={Home}
           />
-          <Route
+           <Route
             path='/countdown'
             exact
             component={Countdown}
+          />
+          <Route
+            path='/score'
+            exact
+            component={Score}
+          />
+           <Route
+            path='/initials'
+            exact
+            component={Initials}
+          />
+          <Route
+            path='/thanks'
+            exact
+            component={Thanks}
           />
         </Switch>
       </BrowserRouter>
