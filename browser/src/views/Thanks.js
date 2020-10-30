d9636aa2b1f22549a8486c4eb39448c4c31bfc59
Dr. Teresa Vasquez <dr.teresavasquez@gmail.com>
2020-10-30 15:18:01 -0500

    added URL

--- browser/src/views/Thanks.js
+++ browser/src/views/Thanks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { Button } from 'reactstrap';
 import UglLogo from '../images/UglLogo.png';
 
@@ -12,7 +13,7 @@ class Thanks extends Component {
           <p className="m-3">
           To learn more about Urban Green Lab, we encourage you to explore our website. There you'll discover our diverse programs, resources for your home or workplace, and more. </p>
           <p>Be sure to check your email to see your game results and more info!</p>
-          <Button className="mt-5">Visit our website</Button>
+          <Button className="mt-5" href='https://urbangreenlab.org/'>Visit our website</Button>
         </div>
       </div>
     );
